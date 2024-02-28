21a708a0ded647ac4ff16a61e88b756296c9c993
Sholto Coulter <sholto.coulter23@students.bhasvic.ac.uk>
2024-02-28 10:00:10 +0000

    parrt 5 penult

---
+++ part5/ex3/src/Apartment.java
@@ -0,0 +1,33 @@
+import java.math.*;
+public class Apartment {
+    private int rooms;
+    private int squares;
+    private int pricePerSquare;
+
+    public Apartment(int rooms, int squares, int pricePerSquare) {
+        this.rooms = rooms;
+        this.squares = squares;
+        this.pricePerSquare = pricePerSquare;
+    }
+    public boolean largerThan(Apartment compared){
+        if(this.squares > compared.squares){
+            return (true);
+        }
+        else if(this.squares < compared.squares){
+            return (false);
+        }
+        return (true);
+    }
+    public int priceDifference(Apartment compared){
+        return(Math.abs((this.squares*this.pricePerSquare)-(compared.squares*compared.pricePerSquare)));
+    }
+    public boolean moreExpensiveThan(Apartment compared){
+        if(this.squares*this.pricePerSquare > compared.pricePerSquare*compared.squares){
+            return (true);
+        }
+        else if(this.pricePerSquare*this.squares < compared.pricePerSquare*compared.squares){
+            return (false);
+        }
+        return (true);
+    }
+}
--- part5/ex3/src/PaymentTerminal.java
+++ part5/ex3/src/PaymentTerminal.java
@@ -8,22 +8,54 @@ public class PaymentTerminal {
     }
 
     public double eatAffordably(double payment) {
-        payment
+        return(calculateChange(2.5, payment));
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty meal and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+        return (calculateChange(4.3, payment));
+    }
+
+    public boolean eatAffordably(PaymentCard card){
+        return(calculateCard(2.5, card));
+    }
+
+    public boolean eatHeartily(PaymentCard card){
+        return(calculateCard(2.5, card));
     }
     public String toString() {
         return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
     }
-    private double payment(double cost, double payment){
-        if(payment<=2.5){
-            affordableMeals++;
+    private boolean calculateCard(double cost, PaymentCard card){
+        if(card.balance() >= cost){
+            switch((int)cost) {
+                case(2):
+                    affordableMeals++;
+                    break;
+                case(4):
+                    heartyMeals++;
+                    break;
+            }
+            card.takeMoney(cost);
+            return (true);
+        }
+        return (false);
+    }
+    public void addedMoneyToCard(PaymentCard card, double money){
+        card.addMoney(money);
+    }
+
+    private double calculateChange(double cost, double payment){
+        if(payment<=cost){
+            switch((int)cost) {
+                case(2):
+                    affordableMeals++;
+                    break;
+                case(4):
+                    heartyMeals++;
+                    break;
+            }
             money += payment;
-            return payment-2.5;
+            return payment-cost;
         }
         else{
             return payment;
